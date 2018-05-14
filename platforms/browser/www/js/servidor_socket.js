547c41c72625453498ab64d6f6c99b6330b23257
Ser-DSTChile\Ser - DST Chile <luis.espinoza@dstchile.cl>
2018-05-14 16:29:35 -0400

    p

--- platforms/browser/www/js/servidor_socket.js
+++ platforms/browser/www/js/servidor_socket.js
@@ -119,9 +119,6 @@ function cierre_servidor(ruta)
 				var wsserver = cordova.plugins.wsserver;	
 					wsserver.stop(function onStop(addr, port) {
 					console.log('Stopped listening on %s:%d', addr, port);
-					$('body').append('<div id="con_pop">Servidor Cerrado</div>');
-					setTimeout(function (){$('#con_pop').fadeOut(1500);},3000);
-					
 					setTimeout(function(){location.href=ruta},100);
 					});
 			}
@@ -137,6 +134,8 @@ function cierre_ser()
 		var wsserver = cordova.plugins.wsserver;	
 			wsserver.stop(function onStop(addr, port) {
 			console.log('Stopped listening on %s:%d', addr, port);
+			$('body').append('<div id="con_pop">Servidor Cerrado</div>');
+			setTimeout(function (){$('#con_pop').fadeOut(1500);},3000);
 			});
 	
 	}
