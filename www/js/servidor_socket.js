d703791dc34f5e4f1fc7071549955dba6ba6b3a3
Ser-DSTChile\Ser - DST Chile <luis.espinoza@dstchile.cl>
2018-05-15 11:38:59 -0400

    p

--- www/js/servidor_socket.js
+++ www/js/servidor_socket.js
@@ -71,6 +71,7 @@ function inicio_proceso(nombre_usuario,numero_telefono)
 				document.getElementById('pop_prueba').innerHTML=sesiones;
 				},
 				'onMessage' : function(conn, msg) {
+					alert(conn.remoteAddr)
 					console.log(conn, msg);
 				mensajes_servidor(wsserver,conn,msg,nombre_usuario,numero_telefono);
 				},
