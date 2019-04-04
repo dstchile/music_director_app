601e31d87c5be4a224114ba1863ab608908d268a
Ser-DSTChile\Ser - DST Chile <luis.espinoza@dstchile.cl>
2019-04-04 00:16:00 -0300

    ggsdfg

--- www/js/servidor_socket.js
+++ www/js/servidor_socket.js
@@ -20,7 +20,7 @@ function getParameterByName(name)
 	}
 
 var wsserver =getParameterByName('wsserver');
-try{var wsserver = JSON.parse(wsserver);}catch(err){}
+/*try{var wsserver = JSON.parse(wsserver);}catch(err){}*/
 var wsserver = wsserver || {};
 alert("wsserver : "+Object.values(wsserver)+"--");
 		
