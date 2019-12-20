887301a0f603d4b726ef8ba0193e819fec85d43a
Ser-DSTChile\Ser - DST Chile <luis.espinoza@dstchile.cl>
2019-12-20 10:14:25 -0300

    plugin de mensajes no funciona

--- www/js/mensajes.js
+++ www/js/mensajes.js
@@ -2,13 +2,14 @@
 
 function mensaje_texto(valor)
 	{
-	navigator.notification.alert(
+	alert(valor)
+	/*navigator.notification.alert(
 						valor,  // message
 						callback,				//llamada a funcion
 						'Mensaje',            // title
 						'OK'                  // buttonName
 					);
-	
+	*/
 	}
 function callback(){};
 				
